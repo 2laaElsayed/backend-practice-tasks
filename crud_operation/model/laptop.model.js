e699092e5771a001b7ec68ee15f3cdb0f9cc7588
Alaa Elsayed <alaaelssayed23@icloud.com>
2025-09-06 14:31:22 +0300

    Update laptop.model.js

--- crud_operation/model/laptop.model.js
+++ crud_operation/model/laptop.model.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const laptopSchema = new Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  price: { type: Number, required: true },
-  isAvailable: { default: true },
-  createdAt: { type: String },
-  updatedAt: { type: String },
-});
+const laptopSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    price: { type: Number, required: true },
+    isAvailable: { type: Boolean, default: true },
+  },
+  { timestamps: true } 
+);
 
 export default model("Laptop", laptopSchema);
